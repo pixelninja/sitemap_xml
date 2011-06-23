f835b3f8afaf3ea873fada02c0dca0096264001c
phillipgray <phill@randb.com.au>
2011-06-23 17:17:12 +1000

    reverted back to content.raw.php to create content...no way to show html elements with XMLElement without it shitting itself

--- assets/sitemap_xml.publish.js
+++ assets/sitemap_xml.publish.js
@@ -1,12 +1,14 @@
 jQuery(function($){
-   // $ = jQuery;
-   // root = $('h6 a').attr('href');
-    
-   // $('pre').load(root + '/symphony/extension/sitemap_xml/raw/');
-   /* $('a[rel=external]').live("click", function () {
+
+   $('pre').load(Symphony.WEBSITE + '/symphony/extension/sitemap_xml/raw/');
+   
+   console.log('hello');
+   console.log(Symphony.WEBSITE);
+   
+    $('a[rel=source]').live("click", function () {
         window.open("view-source:" + $(this).attr('href'));
         return false
-    })*/
+    })
 
 
 
